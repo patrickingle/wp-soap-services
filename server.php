21ffac7e316412813afb24b4a720d4d566fac2ed
Patrick Ingle <inglepatrick@yahoo.com>
2013-05-26 19:31:20 -0400

    Corrected path references

--- server.php
+++ server.php
@@ -3,11 +3,11 @@ include('../../../wp-load.php');
 //define( 'ABSPATH', dirname(__FILE__) . '/');
 define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
 define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' );
-define( 'WP_SOAP_SERVICES_DIR', WPMU_PLUGIN_DIR .  '/wp-soap-services' );
+define( 'WP_SOAP_SERVICES_DIR', WP_PLUGIN_DIR .  '/wp-soap-services' );
 define( 'NUSOAP_DIR', WP_SOAP_SERVICES_DIR . '/lib' );
 
 if (!function_exists('timestamp_to_iso8601')) {
-	include(NUSOAP_DIR . '/lib/nusoap.php');
+	include(NUSOAP_DIR . '/nusoap.php');
 }
 include(WP_SOAP_SERVICES_DIR . '/soap-functions.php');
 include(WP_SOAP_SERVICES_DIR . '/soap-registrations.php');
